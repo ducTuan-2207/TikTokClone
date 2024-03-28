b153dbd36fb77d9f8b105537966c1437108aace8
DucTuan <tuanhypc02@gmail.com>
2024-03-28 14:46:47 +0700

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by macOS on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
